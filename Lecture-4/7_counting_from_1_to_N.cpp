5064934b1862355f4f8dc9a0a1bd8dc666e912cb
namanarora2707 <namanarora270704@gmail.com>
2026-01-13 14:48:17 +0530

    Notes

---
+++ Lecture-4/7_counting_from_1_to_N.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    //for loop
+    for(int i=1;i<=n;i++){
+        cout<<i<<endl;
+    }
+    //while loop
+    int i=1;
+    while(i<=n){
+        cout<<i<<endl;
+        i++;
+    }
+    return 0;
+}
