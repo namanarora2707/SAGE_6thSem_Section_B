76bc71d77f0be28369e3e081453a682ab7bed386
namanarora2707 <namanarora270704@gmail.com>
2026-02-25 15:31:37 +0530

    Notes

--- LeetCode/42.cpp
+++ LeetCode/42.cpp
@@ -6,21 +6,25 @@ int trap(vector<int>& height) {
     int left = 0 , right = n-1;
     int answer = 0;
     int rightmax = 0 , leftmax = 0;
-
-    while (left < right){
-
-        leftmax = max(leftmax , height[left]);
-        rightmax = max(rightmax , height[right]);
-
-        if(leftmax < rightmax){
-
-            answer += leftmax - height[left];
+    while(left<right){
+        if(height[left]<=height[right]){
+            if(leftmax>=height[left]){
+                answer+=leftmax-height[left];
+            }
+            else{
+                leftmax=height[left];
+            }
             left++;
         }
         else{
-            answer += rightmax - height[right];
+            if(rightmax>=height[right]){
+                answer+=rightmax-height[right];
+            }
+            else{
+                rightmax=height[right];
+            }
             right--;
-        }       
+        }
     }
     return answer;
 }
